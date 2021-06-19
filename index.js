553cfb24d7cf2908812ab017be56b0bc1e026887
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-06-19 15:23:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const num1 = 2
 const num2 = 31
 const multiply = num1 * num2
 const random = Math.floor( Math.random() * 1000 ) + 1
-const mod = 16 % 6
+const num3 = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
